05833a3038d42cfa526c886475602d6518de81b0
MariaGuadalupe <mmeza@minsa.gob.ni>
2024-01-23 13:24:01 -0600

    primer commit

---
+++ practica1/bin/practica1.dart
@@ -0,0 +1,5 @@
+import 'package:practica1/practica1.dart' as practica1;
+
+void main(List<String> arguments) {
+  print('Hello world: ${practica1.calculate()}!');
+}
---
+++ practica1/test/practica1_test.dart
@@ -0,0 +1,8 @@
+import 'package:practica1/practica1.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
